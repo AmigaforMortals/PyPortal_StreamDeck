e1e2d1f412a9163b58a95a19e0be4da835f45c75
Amiga for Mortals <127677538+AmigaforMortals@users.noreply.github.com>
2023-03-13 08:44:44 +0000

    Update code.py

--- code.py
+++ code.py
@@ -73,103 +73,95 @@ def key_press(btn_num):
     keyboard.press(btn_num)
     keyboard.release(btn_num)
 
-page_number = 1
+_page_number=1
 
 def load_page_1():
-    if p[1] >160 and p[0] <80:
-        set_image(btn_group,"/images/Buttons2.bmp")
-        #load_page_2()
-    else:
-        # if touch is on top row
-        if p[1] < 80:
-            # Button 1
-            if p[0] <80:
-                key_press(btn1)
-            # Button 2
-            elif p[0] >80 and p[0] <160:
-                key_press(btn2)
-            # Button 3
-            elif p[0] >160 and p[0] <240:
-                key_press(btn3)
-            # Button 4
-            else:
-                key_press(btn4)
-        #if touch is on middle row
-        elif p[1] > 80 and p[1] <160:
-            # Button 5
-            if p[0] <80:
-                key_press(btn5)
-            # Button 6
-            elif p[0] >80 and p[0] <160:
-                key_press(btn6)
-            # Button 7
-            elif p[0] >160 and p[0] <240:
-                key_press(btn7)
-            # Button 8
-            else:
-                key_press(btn8)
-        # if touch is on Bottom row
+    # if touch is on top row
+    if p[1] < 80:
+        # Button 1
+        if p[0] <80:
+            key_press(btn1)
+        # Button 2
+        elif p[0] >80 and p[0] <160:
+            key_press(btn2)
+        # Button 3
+        elif p[0] >160 and p[0] <240:
+            key_press(btn3)
+        # Button 4
         else:
-            # Button 9
-            #if p[0] <80:
-                #key_press(btn9)
-            # Button 10
-            if p[0] >80 and p[0] <160:
-                key_press(btn10)
-            # Button 11
-            elif p[0] >160 and p[0] <240:
-                key_press(btn11)
-            # Button 12
-            else:
-                key_press(btn12)
+            key_press(btn4)
+    #if touch is on middle row
+    elif p[1] > 80 and p[1] <160:
+        # Button 5
+        if p[0] <80:
+            key_press(btn5)
+        # Button 6
+        elif p[0] >80 and p[0] <160:
+            key_press(btn6)
+        # Button 7
+        elif p[0] >160 and p[0] <240:
+            key_press(btn7)
+        # Button 8
+        else:
+            key_press(btn8)
+    # if touch is on Bottom row
+    else:
+        # Button 9
+        #if p[0] <80:
+            #key_press(btn9)
+        # Button 10
+        if p[0] >80 and p[0] <160:
+            key_press(btn10)
+        # Button 11
+        elif p[0] >160 and p[0] <240:
+            key_press(btn11)
+        # Button 12
+        else:
+            key_press(btn12)
 
-"""def load_page_2():
-    if p[1] >160 and p[0] <80:
-        set_image(btn_group,"/images/Buttons.bmp")
-        load_page_1()
-    else:
-        # if touch is on top row
-        if p[1] < 80:
-            # Button 1
-            if p[0] <80:
-                key_press(btn13)
-            # Button 2
-            elif p[0] >80 and p[0] <160:
-                key_press(btn14)
-            # Button 3
-            elif p[0] >160 and p[0] <240:
-                key_press(btn15)
-            # Button 4
-            else:
-                key_press(btn16)
-        #if touch is on middle row
-        elif p[1] > 80 and p[1] <160:
-            # Button 5
-            if p[0] <80:
-                key_press(btn17)
-            # Button 6
-            elif p[0] >80 and p[0] <160:
-                key_press(btn18)
-            # Button 7
-            elif p[0] >160 and p[0] <240:
-                key_press(btn19)
-            # Button 8
-            else:
-                key_press(btn20)
-        # if touch is on Bottom row
+def load_page_2():
+    # if touch is on top row
+    if p[1] < 80:
+        # Button 1
+        if p[0] <80:
+            key_press(btn13)
+        # Button 2
+        elif p[0] >80 and p[0] <160:
+            key_press(btn14)
+        # Button 3
+        elif p[0] >160 and p[0] <240:
+            key_press(btn15)
+        # Button 4
         else:
-            # Button 9
-            #if p[0] <80:
-                #key_press(btn21)
-            # Button 10
-            if p[0] >80 and p[0] <160:
-                key_press(btn22)
-            # Button 11
-            elif p[0] >160 and p[0] <240:
-                key_press(btn23)
-            # Button 12
-            else:
-                key_press(btn24)"""
+            key_press(btn16)
+    #if touch is on middle row
+    elif p[1] > 80 and p[1] <160:
+        # Button 5
+        if p[0] <80:
+            key_press(btn17)
+        # Button 6
+        elif p[0] >80 and p[0] <160:
+            key_press(btn18)
+        # Button 7
+        elif p[0] >160 and p[0] <240:
+            key_press(btn19)
+        # Button 8
+        else:
+            key_press(btn20)
+    # if touch is on Bottom row
+    else:
+        # Button 9
+        #if p[0] <80:
+            #key_press(btn21)
+        # Button 10
+        if p[0] >80 and p[0] <160:
+            key_press(btn22)
+        # Button 11
+        elif p[0] >160 and p[0] <240:
+            key_press(btn23)
+        # Button 12
+        else:
+            key_press(btn24)
 
 # must wait at least this long between touch events
 TOUCH_COOLDOWN = 0.5  # seconds
@@ -195,7 +187,15 @@ while True:
         if p and not _previous_touch:
             # store the time to compare with next iteration
             _previous_touch_time = _now
-            load_page_1()
+	    if p[1] >160 and p[0] <80:
+                if _page_number<=1:
+                    set_image(btn_group,"/images/Buttons2.bmp")
+		    _page_number=2
+        	    load_page_2()
+	        elif _page_number>=2:
+		    set_image(btn_group,"/images/Buttons.bmp")
+		    _page_number=1
+        	    load_page_1()
 
     # store previous touch event t compare with next iteration
     _previous_touch = p
