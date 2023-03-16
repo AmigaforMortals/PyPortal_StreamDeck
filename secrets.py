656521934153f26a1c75c23b7d44279270fc7c52
Amiga for Mortals <127677538+AmigaforMortals@users.noreply.github.com>
2023-03-16 19:01:49 +0000

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : 'YOUR SSID HERE',             # Keep the two '' quotes around the name
+    'password' : 'YOUR PASSWORD HERE',         # Keep the two '' quotes around password
+    'timezone' : "Europe/London",  # http://worldtimeapi.org/timezones
+    'aio_username' : 'YOUR_ADAFRUIT_ACCOUNT_USERNAME',
+    'aio_key' : 'YOUR_ADAFRUITIO_KEY',
+    }
